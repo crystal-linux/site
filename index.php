38a5c2f86b2db957c3c97062f582ad47d08dc6cf
jasio <54361578+jaasio@users.noreply.github.com>
2022-06-21 11:13:20 +0200

    eating cheesy noodles

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
         <div class="container"><a class="navbar-brand" href="#" style="color: #eee;"><img src="assets/img/crystal-logo-minimal.png" width="24" height="24" style="vertical-align:middle;">&nbsp;Crystal Linux</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1" style="border-width: 0px;"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon" style="filter: invert(100%) sepia(4%) saturate(2469%) hue-rotate(188deg) brightness(110%) contrast(87%);"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav me-auto">
-                    <li class="nav-item"><a class="nav-link active" href="https://git.tar.black/crystal" target="_blank" style="pointer-events: auto;">GitLab</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="https://github.com/crystal-linux" target="_blank" style="pointer-events: auto;">GitLab</a></li>
                     <li class="nav-item"><a class="nav-link active" href="https://discord.gg/2b47CAKNAx" target="_blank" style="pointer-events: auto;">Discord</a></li>
                 </ul><span class="navbar-text actions"> <a class="btn btn-light action-button" role="button" href="#" style="border-radius: 6px;background: #a165bf;filter: drop-shadow(0px 5px 5px rgba(161, 101, 191, 0.2));" onclick="window.open(&quot;https://repo.getcryst.al/crystal-latest.iso&quot;);">Download</a></span>
             </div>
@@ -80,7 +80,7 @@
             </div>
             <div class="buttons" style="margin-top: -5px;">
                 <a class="btn btn-primary text-capitalize" role="button" href="#" style="font-family: Inter, sans-serif;border-radius: 6px;font-weight: 500;font-size: 15px;background: #a165bf;filter: drop-shadow(0px 5px 5px rgba(161, 101, 191, 0.2));" onclick="window.open(&quot;https://repo.getcryst.al/crystal-latest.iso&quot;);">Download</a>
-                <button class="btn btn-light text-capitalize" type="button" style="border-radius: 6px;font-size: 15px;font-weight: 500;font-family: Inter, sans-serif;letter-spacing: 0.2px;filter: drop-shadow(0px 5px 5px rgba(221, 221, 221, 0.2));" onclick="window.open(&quot;https://git.tar.black/crystal&quot;);">Gitlab</button>
+                <button class="btn btn-light text-capitalize" type="button" style="border-radius: 6px;font-size: 15px;font-weight: 500;font-family: Inter, sans-serif;letter-spacing: 0.2px;filter: drop-shadow(0px 5px 5px rgba(221, 221, 221, 0.2));" onclick="window.open(&quot;https://github.com/crystal-linux&quot;);">Gitlab</button>
                 </div>
             <div class="card-group" style="margin-top: 100px;filter: drop-shadow(0px 0px 10px rgba(45, 45, 46, 0.2));">
                 <div class="card mobile-compat-card" style="border-radius: 6px;background: #202021;margin-right: 5px;">
@@ -101,9 +101,9 @@
                 <div class="card mobile-compat-card" style="border-radius: 6px;background: #202021;margin-left: 5px;">
                     <div class="card-body">
                         <h4 class="card-title" style="margin-bottom: 2px;color: rgba(238,238,238,0.6);">Contributors</h4>
-                        <p class="card-text" style="color: rgb(142,143,143);">Top five contributors on crystal/ame, jade, branding, and malachite sorted by top commits.</p>
+                        <p class="card-text" style="color: rgb(142,143,143);">Top five contributors on crystal-linux/ame, jade, branding, and malachite sorted by top commits.</p>
                         <div id="ame" style="margin-bottom: 10px;float:left;margin-right:10px">
-                            <p style="margin-bottom: 5px;font-size: 14px;">crystal/ame&nbsp;contributors<br></p>
+                            <p style="margin-bottom: 5px;font-size: 14px;">crystal-linux/ame&nbsp;contributors<br></p>
                             <?php
                                 while($x != 5){
                                     if($ame_obj[$x]->login == null){
@@ -116,7 +116,7 @@
                             ?>
                         </div>
                         <div id="jade" style="margin-bottom: 10px;">
-                            <p style="margin-bottom: 5px;font-size: 14px;">crystal/jade contributors<br></p>
+                            <p style="margin-bottom: 5px;font-size: 14px;">crystal-linux/jade contributors<br></p>
                             <?php
                                 $x = 0;
                                 while($x != 5){
@@ -130,7 +130,7 @@
                             ?>
                         </div>
                         <div id="mal" style="float:left;margin-right:10px">
-                            <p style="margin-bottom: 5px;font-size: 14px;">crystal/malachite&nbsp;contributors<br></p>
+                            <p style="margin-bottom: 5px;font-size: 14px;">crystal-linux/malachite&nbsp;contributors<br></p>
                             <?php
                                 $x = 0;
                                 while($x != 5){
@@ -144,7 +144,7 @@
                             ?>
                         </div>
                         <div id="jade" style="margin-bottom: 10px;">
-                            <p style="margin-bottom: 5px;font-size: 14px;">crystal/branding contributors<br></p>
+                            <p style="margin-bottom: 5px;font-size: 14px;">crystal-linux/branding contributors<br></p>
                             <img style="border-radius: 6px;margin-right: 5px;" width="32" height="32" alt="Merlă Octavian" src="https://git.tar.black/uploads/-/system/user/avatar/15/avatar.png?width=400">
                             <img style="border-radius: 6px;margin-right: 5px;" width="32" height="32" alt="54361578+jaasio@users.noreply.github.com" src="https://secure.gravatar.com/avatar/f0da7fe8afd2d394012e36dbc55059d4?s=800&d=identicon">
                             <img style="border-radius: 6px;margin-right: 5px;" width="32" height="32" alt="lausen@tar.black" src="https://git.tar.black/uploads/-/system/user/avatar/12/avatar.png?width=400">
@@ -167,11 +167,11 @@
                 <div class="card-body">
                     <h4 class="card-title" style="margin-bottom: 2px;color: rgba(238,238,238,0.6);">We 💖 open source software</h4>
                     <p class="card-text" style="color: rgb(142,143,143);">Basically everything that Crystal Linux has coded/made is completely open source for anyone to see. Why not contribute to the project?</p>
-                    <a class="btn btn-primary text-capitalize" role="button" href="#" style="font-family: Inter, sans-serif;border-radius: 6px;font-weight: 500;font-size: 15px;background: #a165bf;filter: drop-shadow(0px 5px 5px rgba(161, 101, 191, 0.2));border:none;outline:none" onclick="window.open(&quot;https://git.tar.black/crystal/&quot;);">Contribute on Gitlab</a>
+                    <a class="btn btn-primary text-capitalize" role="button" href="#" style="font-family: Inter, sans-serif;border-radius: 6px;font-weight: 500;font-size: 15px;background: #a165bf;filter: drop-shadow(0px 5px 5px rgba(161, 101, 191, 0.2));border:none;outline:none" onclick="window.open(&quot;https://github.com/crystal-linux/&quot;);">Contribute on Gitlab</a>
                 </div>
             </div>
             <footer class="text-center">
-                <div class="mobile-footer-fix" style="position: fixed;bottom: 0;margin-bottom: 10px;"><span class="text-muted" style="margin-right: 10px;">Copyright (c) 2022 Crystal Linux</span><a class="link-secondary" href="https://git.tar.black/crystal/site" style="text-decoration:none" target="_blank"><i class="fab fa-gitlab"></i>&nbsp;Source Code</a></div>
+                <div class="mobile-footer-fix" style="position: fixed;bottom: 0;margin-bottom: 10px;"><span class="text-muted" style="margin-right: 10px;">Copyright (c) 2022 Crystal Linux</span><a class="link-secondary" href="https://github.com/crystal-linux/site" style="text-decoration:none" target="_blank"><i class="fab fa-gitlab"></i>&nbsp;Source Code</a></div>
             </footer>
         </div>
     </section>
